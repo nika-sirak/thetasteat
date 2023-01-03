8f542de61182d786111dfe41edb8a8ed0052dfcf
nika-sirak <7nicka@gmail.com>
2023-01-03 16:13:54 +0000

    bugfix swipper

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,87 +1,105 @@
-//refs
-const leftArrow = document.querySelector(".arrow-left");
-const rightArrow = document.querySelector(".arrow-right");
-const frameEl = document.querySelector(".frame");
-const sliderEl = document.querySelector(".slider");
-const sliderItem = document.querySelectorAll(".feedback__li");
-const bottomEl = document.querySelector(".feedback__bottom");
+import Swiper, { Navigation, Pagination } from "swiper";
 
-//var
-let sliderNum = 1;
-const length = sliderItem.length;
-// let widthPx = getComputedStyle(sliderItem[0]).getPropertyValue("width");
-let width = 0;
-
-const sizeObserver = new ResizeObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.contentBoxSize) {
-      const cr = entry.contentRect;
-      width = entry.borderBoxSize[0].inlineSize;
-    }
-  });
+const swiper = new Swiper(".mySwiper", {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  speed: 400,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-sliderItem.forEach((el) => sizeObserver.observe(el));
 
-//radio buttons
-for (let i = 0; i < length; i++) {
-  const div = document.createElement("div");
-  div.classList.add("feedback__btn");
-  bottomEl.appendChild(div);
-}
+// //refs
+// const leftArrow = document.querySelector(".arrow-left");
+// const rightArrow = document.querySelector(".arrow-right");
+// const frameEl = document.querySelector(".frame");
+// const sliderEl = document.querySelector(".slider");
+// const sliderItem = document.querySelectorAll(".feedback__li");
+// const bottomEl = document.querySelector(".feedback__bottom");
 
-const buttonsEl = document.querySelectorAll(".feedback__btn");
-buttonsEl[0].classList.add("feedback__btn-active");
+// //var
+// let sliderNum = 1;
+// const length = sliderItem.length;
+// // let widthPx = getComputedStyle(sliderItem[0]).getPropertyValue("width");
+// let width = 0;
 
-const resetBtnStyle = () => {
-  buttonsEl.forEach((el) => {
-    el.classList.remove("feedback__btn-active");
-  });
-};
+// const sizeObserver = new ResizeObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.contentBoxSize) {
+//       const cr = entry.contentRect;
+//       width = entry.borderBoxSize[0].inlineSize;
+//     }
+//   });
+// });
+// sliderItem.forEach((el) => sizeObserver.observe(el));
 
-buttonsEl.forEach((el, i) =>
-  el.addEventListener("click", () => {
-    resetBtnStyle();
-    sliderEl.style.transform = `translateX(-${i * width}px)`;
-    sliderNum = i + 1;
-    el.classList.add("feedback__btn-active");
-  })
-);
+// //radio buttons
+// for (let i = 0; i < length; i++) {
+//   const div = document.createElement("div");
+//   div.classList.add("feedback__btn");
+//   bottomEl.appendChild(div);
+// }
 
-//arrow slider
-const nextSlide = () => {
-  sliderEl.style.transform = `translateX(-${sliderNum * width}px)`;
-  sliderNum += 1;
-};
+// const buttonsEl = document.querySelectorAll(".feedback__btn");
+// buttonsEl[0].classList.add("feedback__btn-active");
 
-const firstSlide = () => {
-  sliderEl.style.transform = `translateX(0px)`;
-  sliderNum = 1;
-};
+// const resetBtnStyle = () => {
+//   buttonsEl.forEach((el) => {
+//     el.classList.remove("feedback__btn-active");
+//   });
+// };
 
-const prevSlide = () => {
-  sliderEl.style.transform = `translateX(-${(sliderNum - 2) * width}px)`;
-  sliderNum -= 1;
-};
+// buttonsEl.forEach((el, i) =>
+//   el.addEventListener("click", () => {
+//     resetBtnStyle();
+//     sliderEl.style.transform = `translateX(-${i * width}px)`;
+//     sliderNum = i + 1;
+//     el.classList.add("feedback__btn-active");
+//   })
+// );
 
-const lastSlide = () => {
-  sliderEl.style.transform = `translateX(-${(length - 1) * width}px)`;
-  sliderNum = length;
-};
+// //arrow slider
+// const nextSlide = () => {
+//   sliderEl.style.transform = `translateX(-${sliderNum * width}px)`;
+//   sliderNum += 1;
+// };
 
-const changeBtnColor = () => {
-  resetBtnStyle();
-  buttonsEl[sliderNum - 1].classList.add("feedback__btn-active");
-};
+// const firstSlide = () => {
+//   sliderEl.style.transform = `translateX(0px)`;
+//   sliderNum = 1;
+// };
 
-leftArrow.addEventListener("click", onLeftArrow);
-rightArrow.addEventListener("click", onRightArrow);
+// const prevSlide = () => {
+//   sliderEl.style.transform = `translateX(-${(sliderNum - 2) * width}px)`;
+//   sliderNum -= 1;
+// };
 
-function onRightArrow() {
-  sliderNum < length ? nextSlide() : firstSlide();
-  changeBtnColor();
-}
+// const lastSlide = () => {
+//   sliderEl.style.transform = `translateX(-${(length - 1) * width}px)`;
+//   sliderNum = length;
+// };
 
-function onLeftArrow() {
-  sliderNum > 1 ? prevSlide() : lastSlide();
-  changeBtnColor();
-}
+// const changeBtnColor = () => {
+//   resetBtnStyle();
+//   buttonsEl[sliderNum - 1].classList.add("feedback__btn-active");
+// };
+
+// leftArrow.addEventListener("click", onLeftArrow);
+// rightArrow.addEventListener("click", onRightArrow);
+
+// function onRightArrow() {
+//   sliderNum < length ? nextSlide() : firstSlide();
+//   changeBtnColor();
+// }
+
+// function onLeftArrow() {
+//   sliderNum > 1 ? prevSlide() : lastSlide();
+//   changeBtnColor();
+// }
