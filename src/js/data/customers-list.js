7879eb353874d9bd980b2e8f3c277122d37d92c4
nika-sirak <7nicka@gmail.com>
2023-01-03 01:42:40 +0000

    add another customers

---
+++ src/js/data/customers-list.js
@@ -0,0 +1,27 @@
+import customer1 from "../../images/customer1.png";
+import customer2 from "../../images/customer2.png";
+import customer3 from "../../images/customer3.png";
+
+export default [
+  {
+    photo: customer1,
+    name: "Nika D",
+    city: "New York",
+    review:
+      "They known for its fabulous taste and food. Anywhere you go your hunger is satisfied. The best chicken & burgers those are yummy.",
+  },
+  {
+    photo: customer2,
+    name: "Jack Sparrow",
+    city: "Salt Lake City",
+    review:
+      "I have visited this fantastic restaurant on several occasions, food is absolutely outstanding & attention to detail is in league of its own.",
+  },
+  {
+    photo: customer3,
+    name: "Martin",
+    city: "San Diego",
+    review:
+      "Perfectly known for its fabulous taste and food. Anywhere your hunger is satisfied. They give best fried chicken and burgers those are yummy.",
+  },
+];
