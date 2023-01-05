9e94f50fe83ffb437378ff58824552b126974a8c
nika-sirak <7nicka@gmail.com>
2023-01-05 00:06:01 +0000

    add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,34 @@
 const openBtn = document.querySelector('[data-action="open-modal"]');
+const closeBtn = document.querySelector(".close-btn");
 const modal = document.querySelector(".modal");
 
 openBtn.addEventListener("click", onOpenBtn);
+closeBtn.addEventListener("click", onCloseBtn);
 
 function onOpenBtn() {
   modal.showModal();
+  modal.classList.remove("hide");
+  modal.addEventListener("click", onModalClick);
+}
+
+function onCloseBtn() {
+  closeModal();
+}
+
+function onModalClick(e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+}
+
+function closeModal() {
+  modal.classList.add("hide");
+  modal.addEventListener(
+    "animationend",
+    () => {
+      modal.classList.remove("hide");
+      modal.close();
+    },
+    { once: true } // to prevent bugs when reopening the modal
+  );
 }
